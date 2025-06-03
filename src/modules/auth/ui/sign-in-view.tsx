4aa7f806d00b5c8f16be60b0ad083702dc1d50eb
alireza akbarzadeh <akbarzadeh.ali@tapsi.shop>
2025-06-03 11:23:56 +0330

    faet: upload some asses

--- src/modules/auth/ui/sign-in-view.tsx
+++ src/modules/auth/ui/sign-in-view.tsx
@@ -1,12 +1,30 @@
 'use client';
 
-import { Card } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/input";
+import { Form , FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 
 export  function SignInView() {
 
-    return (
-         <Card>
+    const {}=useForm()
 
+    return (
+        <div className="flex flex-col gap-6">
+         <Card className="overflow-hidden p-0">
+            <CardContent className="grid p-0 md:grid-cols-2">
+                <form action="">col 1</form>
+                <div className="bg-radial from-green-500 to-green-800 hidden md:flex flex-col gap-y-4 items-center justify-center">
+                    <img src="/logo.svg" alt="image" className="h-[92px] w-[92px]" />
+                    <p className="text-2xl font-semibold text-white">
+                        Meet.AI
+                    </p>
+                </div>
+            </CardContent>
         </Card>
+        </div>
     )
 }
