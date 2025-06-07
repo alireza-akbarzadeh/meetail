3d4759e2b42c82a9be180472f8e54dee40051917
Alireza Akbrazadeh <akbarzadeh.ali@tapsi.shop>
2025-06-07 18:03:01 +0330

    feat: implement responsive dialog

---
+++ src/components/common/responsive-dialog.tsx
@@ -0,0 +1,58 @@
+'use client';
+import { useIsMobile } from '@/hooks/use-mobile';
+import { ReactNode } from 'react';
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+
+interface ResponsiveDialogProps {
+  title: string;
+  description: string;
+  children: ReactNode;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+export function ResponsiveDialog(props: ResponsiveDialogProps) {
+  const { title, description, children, open, onOpenChange } = props;
+  const isMobile = useIsMobile();
+
+  if (isMobile) {
+    return (
+      <Drawer open={open} onOpenChange={onOpenChange}>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>{title}</DrawerTitle>
+            <DrawerDescription>{description}</DrawerDescription>
+          </DrawerHeader>
+          <div className="px-4">{children}</div>
+        </DrawerContent>
+      </Drawer>
+    );
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        {children}
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/modules/agents/views/agents-view.tsx
+++ src/modules/agents/views/agents-view.tsx
@@ -1,9 +1,30 @@
 'use client';
 import { useTRPC } from '@/trpc/client';
 import { useSuspenseQuery } from '@tanstack/react-query';
+import { ResponsiveDialog } from '@/components/common/responsive-dialog';
+import { Button } from '@/components/ui/button';
+import { useState } from 'react';
 
 export function AgentsView() {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(trpc.agents.geMany.queryOptions());
-  return <div>a{JSON.stringify(data, null, 2)}</div>;
+  const [open, setOpen] = useState<boolean>(false);
+
+  return (
+    <div>
+      <Button onClick={() => setOpen((open) => !open)}>open dialog</Button>
+      <ResponsiveDialog
+        title={'title test'}
+        description={'description test'}
+        open={open}
+        onOpenChange={setOpen}
+      >
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti eaque est perspiciatis
+          sequi ut.
+        </p>
+      </ResponsiveDialog>
+      {JSON.stringify(data, null, 2)}
+    </div>
+  );
 }
